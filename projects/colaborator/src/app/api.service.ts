f6479b93cd39f2d7a91a8cbf936ea1521d7a953f
Nelson Aguiar <nelson.aguiar@b-simple.pt>
2024-05-06 11:51:13 +0100

    done?

---
+++ projects/colaborator/src/app/api.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private apiUrl = 'https://localhost:5001'; // Replace with your API endpoint
+  private apiUrl2 = 'https://localhost:5011';
+
+
+
+  constructor(private http: HttpClient) {}
+
+
+  // Example GET request
+  getItems() {
+    return this.http.get(`${this.apiUrl}/api/Colaborator`);
+  }
+  // Example POST request
+  createItem(item: any) {
+    return this.http.post(`${this.apiUrl2}/api/Colaborator`, item);
+  }
+}
--- projects/colaborator/src/app/home/home.component.ts
+++ projects/colaborator/src/app/home/home.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  items: any[] = [];
+  filteredItems: any[] = [];
+  searchTerm: string = '';
 
+  constructor(private apiService: ApiService) {}
+  ngOnInit() {
+    this.fetchItems();
+  }
+
+  fetchItems() {
+    if (this.apiService) {
+      this.apiService.getItems().subscribe((response) => {
+        console.log('Items:', response);
+        this.items = response as any[];
+        this.filteredItems = this.items;
+      });
+    }
+  }
+  searchCollaborators() {
+    this.filteredItems = this.items.filter(item =>
+      item.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  createNewItem(formData: any) {
+    const newItem = {
+      email: formData.email,
+      name: formData.name,
+      street: formData.address,
+      postalCode: formData.postalCode
+    };
+    this.apiService.createItem(newItem).subscribe((response) => {
+      console.log('Created item:', response);
+      // Handle success or any UI updates here
+    });
+  }
 }
